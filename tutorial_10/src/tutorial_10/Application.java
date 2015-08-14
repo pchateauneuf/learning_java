d61cfac80f285af8898dd45d2d05bf2f46dfb430
Peter Chateauneuf <peter.chateauneuf@gmail.com>
2015-08-14 14:56:16 -0400

    comming t10

---
+++ tutorial_10/src/tutorial_10/Application.java
@@ -0,0 +1,38 @@
+package tutorial_10;
+
+public class Application {
+	public static void main(String[] args) {
+		
+		// CREATE ARRAY
+		int[] values;
+		
+		values = new int[3];
+		
+		values[0] = 10;
+		values[1] = 20;
+		values[2] = 30;
+		
+//		System.out.println(values[0]);
+//		System.out.println(values[1]);
+//		System.out.println(values[2]);
+		
+		System.out.println("values of 'value' array:");
+		
+		for(int i = 0; i < values.length; i++){
+			System.out.println(values[i]);
+		} // for
+		
+		int[] numbers = {5,6,7};
+		
+		System.out.println("values of 'numbers' array:");
+		
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		} // for
+		
+		System.out.println("total count of 'numbers' array:");
+		
+		System.out.println(numbers.length);
+		
+	}
+}
